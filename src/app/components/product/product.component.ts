86f72da126ac1c2ebb4e67f0ce428dac58489b2b
DanielAristy <danielariaty@gmail.com>
2020-02-05 16:29:01 -0500

    Ciclo de vida de los componentes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges, 
+  DoCheck,
+  OnDestroy
+} from '@angular/core';
 import { Product} from '../../model/product';
 import { from } from 'rxjs';
 
@@ -7,16 +17,34 @@ import { from } from 'rxjs';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements  OnInit, DoCheck, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+ 
   @Input() product: Product;
 
   /**Creando un evento */
   @Output() productClicked: EventEmitter<any> = new EventEmitter;
+
+  constructor() { 
+    console.log('1. constructor');
+  }
+  /**Dectar los cambios 
+  ngOnChanges(changes: SimpleChange){
+    console.log('2. ngOnChanges');
+    console.log(changes);
+  } */
+
+  ngOnInit(){
+    console.log('3. ngOnInit');
+  }
+
+  ngDoCheck(){
+    console.log('4. ngDoCheck');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
+  }
 
   addCar(){
     console.log('Agregar al carrito');
